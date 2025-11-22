241d4495015412c172c85d627709398888dc658d
Pedro Alves <pedro-alves.pa@academico.ifpb.edu.br>
2025-11-22 17:07:25 -0300

    Alterado.

--- front/js/tablelistconfig.js
+++ front/js/tablelistconfig.js
@@ -6,6 +6,7 @@ const family_filter = document.getElementById('familyFilter')
 const reset_filters_btn = document.getElementById('btnReset')
 const search_qtd = document.getElementById('totalCount')
 const qtd_filter = document.getElementById('filteredCount')
+const search_button = document.getElementById('btnSearch')
 
 async function tables_table_body() {
   const tables = await get('tables')
@@ -101,4 +102,62 @@ async function applycount() {
 
 applycount()
 
+async function applycountfilter() {
+    const tables = await get ('tables')
+    let count = 0
+    const search_family = family_filter.value
+    if (search_family === 'all'){
+        for (const index in tables) {
+            count += 1
+        }
+        
+    }
+    else {
+        for (const index in tables){
+            if (tables[index].family === search_family){
+                count += 1
+            }
+        }
+    }
+    qtd_filter.innerText = count
+}
+
+family_filter.addEventListener('change', applycountfilter) 
+reset_filters_btn.addEventListener('click', applycountfilter)
+
+
+async function searchTables() {
+    const tables = await get('tables')
+    const search_term = search_input.value.toLowerCase()
+    table_body.innerHTML = ''
+    for (const index in tables) {
+        if (tables[index].name.toLowerCase().includes(search_term) ||
+            tables[index].family.toLowerCase().includes(search_term) ||
+            tables[index].description.toLowerCase().includes(search_term)) {
+            loadLine(tables, 'tables', index, table_body,
+            ['name', 'family', 'description'])
+        }
+        
+    }
+    
+}
+
+search_button.addEventListener('click', searchTables)
+
+async function search_name_qtd(){
+    const tables = await get('tables')
+    const search_term = search_input.value.toLowerCase()
+    let count = 0
+    for (const index in tables) {
+        if (tables[index].name.toLowerCase().includes(search_term) ||
+            tables[index].family.toLowerCase().includes(search_term) ||
+            tables[index].description.toLowerCase().includes(search_term)) {
+            count += 1
+        }
+
+    }
+    qtd_filter.innerText = count
+}
+
+search_button.addEventListener('click', search_name_qtd)
 
