7ff133f8314afecf380e2828f6781428bcd2284d
icaruuuuuu <icaromachadosilva@gmail.com>
2025-11-22 23:12:53 -0300

    overlay bonitificado no tablelist

--- front/js/overlay.js
+++ front/js/overlay.js
@@ -1,25 +1,29 @@
 function toggleOverlay(){
 	const overlay = document.querySelector('.overlay');
+	document.body.classList.toggle('blur');
 	overlay.classList.toggle('active');
 }
 
 function leaveOverlay(){
 	const overlay = document.querySelector('.overlay');
+	document.body.classList.remove('blur');
 	overlay.classList.remove('active');
 }
 
 const iframe = document.getElementById('frame');
 iframe.onload = () => {
 	const iframeDoc = iframe.contentDocument || iframe.contentWindow.document;
-	iframeDoc.querySelector('.btn-cancel').addEventListener('click', () => {
-		leaveOverlay();
-	});
+	const cancelBtn = iframeDoc.querySelector('.btn-cancel');
+
+	if (cancelBtn) {
+		cancelBtn.addEventListener('click', () => {
+			leaveOverlay();
+		});
+	}
 }
 
 document.addEventListener('keydown', (e) => {
-	if (e.key == "Escape") {
+	if (e.key === "Escape") {
 		leaveOverlay();
 	}
-})
-
-
+});
