e7fe7cef3fa76925f0bec919941b2a2ba25b1abf
icaruuuuuu <icaromachadosilva@gmail.com>
2026-02-05 02:28:42 +0000

    rota de reset da config do nftables

--- back/src/routes/submitRoutes.js
+++ back/src/routes/submitRoutes.js
@@ -24,6 +24,36 @@ router_submit.get('/submit', async (req, res) => {
 
 })
 
+router_submit.get('/reset', async (req, res) => {
+	
+	const TEMP = '/tmp'
+	const data = await All.readAll()
+	const config = `#!/usr/sbin/nft -f
+
+	flush ruleset
+
+	table inet filter {
+	    chain input {
+		type filter hook input priority filter;
+	    }
+	    chain forward {
+		type filter hook forward priority filter;
+	    }
+	    chain output {
+		type filter hook output priority filter;
+	    }
+	}`;
+	
+	const file = path.join(TEMP, 'app.conf')
+	fs.writeFile(file, config, (error) => {
+		if (error) {
+			res.status(500).send('Failed to submit configuration: ', error)
+		} else {
+			res.status(200).json({})
+		}
+	});
+})
+
 export { router_submit }
 
 // const config = await All.readAll()
